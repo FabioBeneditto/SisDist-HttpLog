003946d7db3069d69e959acfb71e1063f7d4e857
Fabio Beneditto <fabiobeneditto@gmail.com>
2015-08-13 22:02:02 -0300

    Adicionado retorno do cabeçalho

--- src/ServerHttp.java
+++ src/ServerHttp.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by fabio on 13/08/15.
@@ -7,7 +10,13 @@ public class ServerHttp {
     public static void main(String[] args){
         try {
             ServerSocket ss = new ServerSocket(8080);
-            ss.accept();
+            Socket s = ss.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            String row;
+            while((row = in.readLine()).length() > 0){
+                System.out.println(row);
+            }
 
         } catch (Exception e){
             e.printStackTrace();
